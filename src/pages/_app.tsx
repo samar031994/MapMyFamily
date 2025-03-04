53fdeaef111addf75b1eb3d91ffdcd328ee349b6
Samar Manjeshwar <sam.manjeshwar@gmail.com>
2025-03-03 21:23:32 -0500

    Add basic Oauth with google

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
-import type { AppProps } from 'next/app'
-import '../global.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-const App = ({ Component, pageProps }: AppProps) =>{
-    console.log("_app")
-    return <Component {...pageProps} />
-}
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import "../global.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
 
-export default App
+export default App;
